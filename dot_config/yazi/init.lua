fe3201d03005ef7952f058f10060d117fb09430d
Kevin Ly <kevin.ly24@gmail.com>
2025-03-30 16:54:17 -0400

    adding kitty config, moved to yazi pack for plugins removed tmux rename hook

--- dot_config/yazi/init.lua
+++ dot_config/yazi/init.lua
@@ -2,3 +2,14 @@ require("full-border"):setup({
 	-- Available values: ui.Border.PLAIN, ui.Border.ROUNDED
 	type = ui.Border.ROUNDED,
 })
+
+require("bunny"):setup({
+  hops = {
+    { key = "h",          path = "~", desc = "Home"},
+    { key = "i",          path = "~/Library/Mobile Documents/com~apple~CloudDocs", desc = "iCloud"},
+    { key = "c",          path = "~/.config",      desc = "Config files" },
+  },
+  desc_strategy = "path", -- If desc isn't present, use "path" or "filename", default is "path"
+  notify = false, -- Notify after hopping, default is false
+  fuzzy_cmd = "fzf", -- Fuzzy searching command, default is "fzf"
+})
